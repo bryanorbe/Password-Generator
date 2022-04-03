259c3c8b247cfef29fc003611a8a6b83f8067867
Bryan Orbe <bryan.orbe@gmail.com>
2022-04-03 03:46:37 -0400

    fixed the arrays and added prompts for them

--- Assets/script.js
+++ Assets/script.js
@@ -12,3 +12,59 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//  create password
+
+function createPassword() {
+
+  userPassword = ""
+
+//add all possible characters
+
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+  var numbers = "1234567890"
+  var symbols = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+  var passwordCharacters = "";
+  
+ 
+  var lowerConfirm = confirm("Include lowercase letters?")
+  if (lowerConfirm) {
+    passwordCharacters += lowerCase
+  } else {
+    passwordCharacters = passwordCharacters
+  }
+
+  var upperConfirm = confirm("Include uppercase letters?")
+  if (upperConfirm) {
+    passwordCharacters += upperCase
+  } else {
+    passwordCharacters = passwordCharacters
+  } 
+
+  var numberConfirm = confirm("Include numeric characters?")
+  if (numberConfirm) {
+    passwordCharacters += numbers
+  } else {
+    passwordCharacters = passwordCharacters
+  }
+
+  var symbols = confirm("Include special characters?")
+  if (specialConfirm) {
+    passwordCharacters += symbols
+  } else {
+    passwordCharacters = passwordCharacters
+  }
+
+
+}
+
+var confirmUpperChar = confirm("Add Uppercase Letters?")
+    if (confirmUpperChar == true) {
+        selectedTypes = selectedTypes.concat(uppercase);
+        else
+    }
+
+  passwordText.value = password;
+
+}
